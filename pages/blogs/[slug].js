a5291a52692c739c9060af1558ade99cd8bc48e0
MyoMyintAung <myomyintaung064@gmail.com>
2022-06-18 17:33:26 +0630

    define routing for 4 core pages

---
+++ pages/blogs/[slug].js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BlogDetailPage = () => {
+  return <h1>this is blog detail page</h1>;
+};
+
+export default BlogDetailPage;
---
+++ pages/blogs/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AllBlogsPage = () => {
+  return (
+    <div>
+      <h1>this is all blogs page</h1>
+    </div>
+  );
+};
+
+export default AllBlogsPage;
