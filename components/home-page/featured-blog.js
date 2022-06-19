f632b75878cc1ba59a9c90d81f28f43815f2de19
MyoMyintAung <myomyintaung064@gmail.com>
2022-06-19 13:23:35 +0630

    added post-grid + style

---
+++ components/home-page/featured-blog.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./featured-blog.module.css";
+
+const FeatureBlogs = () => {
+  return (
+    <div className={classes.latest}>
+      <h1>Featured blog posts</h1>
+    </div>
+  );
+};
+
+export default FeatureBlogs;
