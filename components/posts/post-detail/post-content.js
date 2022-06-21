ff35e8271b4ca6bb80477703ac923743aa26fc9b
MyoMyintAung <myomyintaung064@gmail.com>
2022-06-21 11:21:06 +0630

    rendered mark-down pngs and code snippets with NextJs/Image

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -4,37 +4,47 @@ import PostHeader from "./post-header";
 import classes from "./post-content.module.css";
 import ReactMarkdown from "react-markdown";
 
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { dracula } from "react-syntax-highlighter/dist/cjs/styles/prism";
+
 const PostContent = (props) => {
   const { post } = props;
   const imagePath = `/image/posts/${post.slug}/${post.image}`;
 
+  const customRenderers = {
+    p(paragraph) {
+      const { node } = paragraph;
+
+      if (node.children[0].tagName === "img") {
+        const image = node.children[0];
+        return (
+          <Image
+            src={`/image/posts/${post.slug}/${image.properties.src}`}
+            alt={image.properties.alt}
+            width={600}
+            height={300}
+          />
+        );
+      }
+
+      return <p>{paragraph.children}</p>;
+    },
+
+    code(code) {
+      const { className, children } = code;
+      const language = className.split("-")[1];
+      return (
+        <SyntaxHighlighter style={dracula} language={language}>
+          {children}
+        </SyntaxHighlighter>
+      );
+    },
+  };
+
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown
-        components={{
-          p: ({ node, children }) => {
-            if (node.children[0].tagName === "img") {
-              const image = node.children[0];
-              return (
-                <div className={classes.image}>
-                  <Image
-                    className={classes.image}
-                    src={`/image/posts/${post.slug}/${image.properties.src}`}
-                    alt={image.properties.alt}
-                    width="600"
-                    height="300"
-                  />
-                </div>
-              );
-            }
-            // Return default child if it's not an image
-            return <p>{children}</p>;
-          },
-        }}
-      >
-        {post.content}
-      </ReactMarkdown>
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
     </article>
   );
 };
