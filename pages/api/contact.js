df8a64e585c5bf7a624df3e5a62eaa5ea7ffb6ea
MyoMyintAung <myomyintaung064@gmail.com>
2022-06-21 16:16:43 +0630

    added error handling to api route

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -13,7 +13,7 @@ const handler = async (req, res) => {
       !message ||
       message.trim().length === 0
     ) {
-      res.status(400).json({ message: "Invalid input" });
+      res.status(422).json({ message: "Invalid input" });
       return;
     }
 
@@ -23,14 +23,32 @@ const handler = async (req, res) => {
       text: message,
     };
 
-    const client = await MongoClient.connect(
-      "mongodb+srv://myomyintaung:fnztBRvFTIOAzodL@cluster0.zq497.mongodb.net/?retryWrites=true&w=majority"
-    );
+    let client;
 
-    const db = client.db("blog");
+    try {
+      client = await MongoClient.connect(
+        "mongodb+srv://myomyintaung:fnztBRvFTIOAzodL@cluster0.zq497.mongodb.net/?retryWrites=true&w=majority"
+      );
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: error.message || "Connecting to server failed!" });
+      client.close();
+      return;
+    }
 
-    await db.collection("contacts").insertOne({ ...contact });
+    const db = client.db("blog"); // database name
+
+    try {
+      await db.collection("contacts").insertOne({ ...contact }); // collection name
+    } catch (error) {
+      res.status(402).json({ message: "Inserting to database failed." });
+      client.close();
+      return;
+    }
+
     res.status(201).json({ message: "Message sent successfully" });
+    client.close();
   }
 };
 export default handler;
