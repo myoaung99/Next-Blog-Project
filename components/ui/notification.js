c610a9e70fc0d075aa520ed72585050c3bd2edbd
MyoMyintAung <myomyintaung064@gmail.com>
2022-06-23 17:41:17 +0630

    notification animation

--- components/ui/notification.js
+++ components/ui/notification.js
@@ -1,6 +1,29 @@
 import ReactDOM from "react-dom";
-
+import { motion, AnimatePresence } from "framer-motion";
 import classes from "./notification.module.css";
+
+const noti = {
+  hidden: {
+    y: 500,
+    opacity: 0,
+  },
+  visible: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      type: "spring",
+      stiffness: 120,
+      duration: 1,
+    },
+  },
+
+  exit: {
+    y: 500,
+    transition: {
+      duration: 0.7,
+    },
+  },
+};
 
 function Notification(props) {
   const { title, message, status } = props;
@@ -18,10 +41,18 @@ function Notification(props) {
   const cssClasses = `${classes.notification} ${statusClasses}`;
 
   return ReactDOM.createPortal(
-    <div className={cssClasses}>
-      <h2>{title}</h2>
-      <p>{message}</p>
-    </div>,
+    <AnimatePresence>
+      <motion.div
+        variants={noti}
+        initial="hidden"
+        animate="visible"
+        exit="exit"
+        className={cssClasses}
+      >
+        <h2>{title}</h2>
+        <p>{message}</p>
+      </motion.div>
+    </AnimatePresence>,
     document.getElementById("notifications")
   );
 }
